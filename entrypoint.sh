00495d67d06bca06b1c4a2c213812ff6d0cca44a
Fabian Köstring <FabianKoestring@users.noreply.github.com>
2019-09-22 18:44:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,7 +80,7 @@ main() {
 
     # handle issues
     if [[ "$GITHUB_EVENT_NAME" == "issues" && "$GITHUB_EVENT_ACTION" == "opened" ]]; then
-        GITHUB_ISSUE_EVENT_NUMBER=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
+        GITHUB_ISSUE_EVENT_NUMBER=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
         GITHUB_ISSUE_EVENT_BODY=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
 
         if [[ "$GITHUB_ISSUE_EVENT_BODY" =~ $ISSUE_PATTERN ]]
